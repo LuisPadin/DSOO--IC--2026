51780dcd8ab0de15793fcf08f93f7937bda3b7ec
LuisPadin <padin667@gmail.com>
2026-06-03 12:51:26 -0300

    Update Prestamo.py

--- Prestamo.py
+++ Prestamo.py
@@ -2,9 +2,9 @@ class Prestamo:
     contador_prestamo = 0
 
     def __init__(self, socio, material):
-        # ID autoincremental para el préstamo
+        # ID autoincremental numérico puro para el préstamo
         Prestamo.contador_prestamo = Prestamo.contador_prestamo + 1
-        self.id_prestamo = "Prestamo" + str(Prestamo.contador_prestamo)
+        self.id_prestamo = str(Prestamo.contador_prestamo)
         self.socio = socio
         self.material = material
         self.activo = True
@@ -15,4 +15,4 @@ class Prestamo:
         self.material.devolver()
 
     def __str__(self):
-        return "[" + self.id_prestamo + "] Socio: " + self.socio.nombre + " (" + self.id_socio + ") -> Material: " + self.material.titulo
+        return "[Préstamo N° " + self.id_prestamo + "] Socio: " + self.socio.nombre + " (ID: " + self.socio.id_socio + ") -> Material: " + self.material.titulo
