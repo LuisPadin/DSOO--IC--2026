dcf98a4ffe0701944877ea50f8bdfe621ac35dee
LuisPadin <padin667@gmail.com>
2026-06-03 16:46:57 -0300

    Update Material.py

--- Material.py
+++ Material.py
@@ -1,10 +1,11 @@
 class Material:
     contador_material = 0
 
-    def __init__(self, titulo):
+    def __init__(self, titulo, año):
         Material.contador_material = Material.contador_material + 1
         self.id_material = str(Material.contador_material)
-        self.titulo = title = titulo
+        self.titulo = titulo
+        self.año = año
         self.disponible = True
 
     def prestar(self):
@@ -24,19 +25,20 @@ class Material:
         return "[" + self.id_material + "] " + self.titulo + " - " + estado
 
 class Libro(Material):
-    def __init__(self, titulo, autor):
-        super().__init__(titulo)
+    def __init__(self, titulo, autor, año):
+        super().__init__(titulo, año)
         self.autor = autor
         
     def __str__(self):
-        return super().__str__() + " | Tipo: Libro | Autor: " + self.autor
+        return super().__str__() + " | Tipo: Libro | Autor: " + self.autor + " | Año: " + str(self.año)
 
 class Revista(Material):
-    def __init__(self, titulo, numero):
-        super().__init__(titulo)
+    def __init__(self, titulo, numero, año):
+        super().__init__(titulo, año)
         self.numero = numero
         
     def __str__(self):
-        return super().__str__() + " | Tipo: Revista | N°: " + str(self.numero)
+        return super().__str__() + " | Tipo: Revista | N°: " + str(self.numero)  + " | Año: " + str(self.año)
+
 
 
