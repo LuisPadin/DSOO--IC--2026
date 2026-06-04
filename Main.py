439244e49ebc1b48011f2c4b6c20d6c60d0b51ac
LuisPadin <padin667@gmail.com>
2026-06-04 10:08:21 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -14,13 +14,13 @@ def menu_principal():
         print("\n" )
         print(" ⚔️BIBLIOTECA⚔️ ")
         print("\n" )
-        print("1| Alta de Material")
-        print("2| Alta de Socio")
-        print("3| Consultar Inventario")
-        print("4| Registrar Préstamo")
-        print("5| Registrar Devolución")
+        print("1| Agregar Libro o Revista")
+        print("2| Agregar Socio")
+        print("3| Ver Inventario")
+        print("4| Realizar Préstamo")
+        print("5| Realizar Devolucion")
         print("6| Consultar Préstamos")
-        print("7| Ver Listado de Socios")
+        print("7| Ver Socios")
         print("8| Ver Socios Habilitados")
         print("0| Salir")
         
@@ -28,35 +28,45 @@ def menu_principal():
         opcion = input("Seleccione una opción: ")
 
         if opcion == "1":
-            tipo = input("¿Tipo de material? (1: Libro / 2: Revista): ")
+            tipo = input("(1: Libro / 2: Revista): ")
             titulo = input("Título: ")
             if tipo == "1":
                 autor = input("Autor: ")
                 año = int(input( "Año: "))
                 biblioteca.agregar_material(Libro(titulo, autor, año))
+                print("\n" )
+                print( "Libro agregado.")
             elif tipo == "2":
                 edicion = int(input("Número de edición: "))
                 año = int(input( "Año: "))
                 biblioteca.agregar_material(Revista(titulo, edicion, año))
+                print("\n" )
+                print("Revista agregada.")
             else:
+                print("\n" )
                 print("Opción inválida.")
 
         elif opcion == "2":
-            nom = input("Nombre y Apellido del Socio: ")
+            nom = input("Nombre y Apellido: ")
             biblioteca.agregar_socio(Socio(nom))
+            print("\n" )
+            print("Socio agregado")
 
         elif opcion == "3":
             biblioteca.mostrar_inventario()
 
         elif opcion == "4":
-            # Ahora los IDs son números súper cortos
             socio_id = input("ID numérico del socio (Ej: 1): ")
             mat_cod = input("ID numérico del material (Ej: 1): ")
             biblioteca.realizar_prestamo(socio_id, mat_cod)
+            print("\n" )
+            print("Prestamo realizado")
 
         elif opcion == "5":
             mat_cod = input("ID numérico del material a devolver (Ej: 1): ")
             biblioteca.realizar_devolucion(mat_cod)
+            print("\n" )
+            print("Devolucion realizada")
 
         elif opcion == "6":
             biblioteca.mostrar_prestamos_activos()
@@ -68,12 +78,10 @@ def menu_principal():
             biblioteca.mostrar_habilitados()
 
         elif opcion == "0":
-            print("¡Gracias por utilizar el sistema!")
+            print("Chau Crack")
             break
         else:
             print("Opción no válida. Intente nuevamente.")
 
-if __name__ == "__main__":
-    menu_principal()
 if __name__ == "__main__":
     menu_principal()
