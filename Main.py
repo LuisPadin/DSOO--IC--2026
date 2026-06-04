8949fe1b2a1b50f78658ac971303225f0d3cdade
LuisPadin <padin667@gmail.com>
2026-06-04 08:50:09 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,41 +1,43 @@
 from Biblioteca import Biblioteca
-from Material import Libro, Revista
+from Material import Libro, Revista      
 from Socio import Socio
 
 def menu_principal():
     biblioteca = Biblioteca()
     
-    # Datos de prueba 
-    biblioteca.agregar_material(Libro("El Aleph", "Jorge Luis Borges"))
-    biblioteca.agregar_material(Revista("National Geographic", 520))
+   
+    biblioteca.agregar_material(Libro("El Aleph", "Jorge Luis Borges", "2005"))
+    biblioteca.agregar_material(Revista("National Geographic", 520, "2005"))
     biblioteca.agregar_socio(Socio("Juan Pérez"))
 
     while True:
-        print("\n" + "="*40)
-        print("   SISTEMA DE GESTIÓN - BIBLIOTECA IFTS 11")
-        print("="*40)
-        print("1. Alta de Material")
-        print("2. Alta de Socio")
-        print("3. Consultar Inventario Completo")
-        print("4. Registrar Préstamo")
-        print("5. Registrar Devolución")
-        print("6. Consultar Préstamos Activos")
-        print("7. Ver Listado de Socios")
-        print("8. Ver Socios Habilitados")
-        print("0. Salir")
-        print("-"*40)
+        print("\n" )
+        print(" ⚔️BIBLIOTECA⚔️ ")
+        print("\n" )
+        print("1| Alta de Material")
+        print("2| Alta de Socio")
+        print("3| Consultar Inventario")
+        print("4| Registrar Préstamo")
+        print("5| Registrar Devolución")
+        print("6| Consultar Préstamos")
+        print("7| Ver Listado de Socios")
+        print("8| Ver Socios Habilitados")
+        print("0| Salir")
+        
         
         opcion = input("Seleccione una opción: ")
 
         if opcion == "1":
             tipo = input("¿Tipo de material? (1: Libro / 2: Revista): ")
-            tit = input("Título: ")
+            titulo = input("Título: ")
             if tipo == "1":
-                aut = input("Autor: ")
-                biblioteca.agregar_material(Libro(tit, aut))
+                autor = input("Autor: ")
+                año = int(input( "Año: "))
+                biblioteca.agregar_material(Libro(titulo, autor, año))
             elif tipo == "2":
                 edicion = int(input("Número de edición: "))
-                biblioteca.agregar_material(Revista(tit, edicion))
+                año = int(input( "Año: "))
+                biblioteca.agregar_material(Revista(titulo, edicion, año))
             else:
                 print("Opción inválida.")
 
@@ -71,5 +73,7 @@ def menu_principal():
         else:
             print("Opción no válida. Intente nuevamente.")
 
+if __name__ == "__main__":
+    menu_principal()
 if __name__ == "__main__":
     menu_principal()
